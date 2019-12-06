96170f5d68aeee62a4b53c968b1443ab91575697
root <root@Maruka.localdomain>
2019-12-06 10:24:36 -0600

    MICROPOST SCAFFOLD

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_05_204829) do
+ActiveRecord::Schema.define(version: 2019_12_06_154855) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
